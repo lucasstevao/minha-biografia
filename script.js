83adfc32a73cf7806a29274143c5c6b21847c401
lucasstevao <stevao.oc@gmail.com>
2025-06-28 00:00:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let botao = document.querySelector(".botaojs");
+let msg = document.querySelector(".msgbotaojs");
+
+botao.addEventListener("click", function() {
+    msg.style.display="block";
+});
